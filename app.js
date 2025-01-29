35c81636a367c881fe9ab417f9a9390592768d14
rcSabucido <rcsabucido@gmail.com>
2025-01-29 19:19:11 +0800

    Add dynamic year change to footer on app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const heroButton = document.querySelector('.hero-button');
 const exploreElement = document.querySelector('#project-title');
+const currentYear = new Date().getFullYear();
+const footer = document.querySelector('.footer-text');
+
+footer.textContent = `Copyright © ${currentYear} by Ryz Clyd Sabucido`;
 
 heroButton.addEventListener('click', () => {
     exploreElement.scrollIntoView({ behavior: 'smooth' });
